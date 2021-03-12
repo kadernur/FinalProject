d1e2d8110c8132e7cffc8a212896faa128ba2945
kaderNurTekin <tekinkadernur@gmail.com>
2021-03-12 15:37:38 +0300

    14.gün sonu

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,6 +1,8 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -16,5 +18,12 @@ namespace Business.Constants
         public static string ProductCountOfCategoryError = "Kategoride en fazla 10 ürün olabilir.";
         public static string ProductNameAlreadyExists="Bu isimde zaten başka bir ürün var.";
         public static string CategoryLimitExceded="Category limiti aşıldı. Yeni ürün eklenmiyor.";
+        public static string AuthorizationDenied="Yetkiniz yok.";
+        public  static string UserRegistered="Kayıt oldu.";
+        public  static string UserNotFound="Kullanıcı bulunamadı.";
+        public  static string PasswordError="Parola hatalı";
+        public static string SuccessfulLogin="Giriş başarılı";
+        public  static string UserAlreadyExists="Kullanıcı mevcut.";
+        public  static string AccessTokenCreated="Token oluşturuldu.";
     }
 }
---
+++ Core/Utilities/Security/Encryption/SigningCredentialsHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.Encryption
+{
+   public  class SigningCredentialsHelper
+    {
+        //web api nin tookenları kullanması için gerekli anahtar.
+        public static SigningCredentials CreateSigningCredentials(SecurityKey securityKey)
+        {
+            return new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512Signature);
+
+
+        }
+    }
+}
